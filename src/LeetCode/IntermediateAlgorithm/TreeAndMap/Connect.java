da6486a036a16f4580c0becf3aa0d99703211f3b
bert_cai <bert_cai@163.com>
2018-11-12 12:17:35 +0800

    创建慕课网算法学习文件夹

---
+++ src/LeetCode/IntermediateAlgorithm/TreeAndMap/Connect.java
@@ -0,0 +1,35 @@
+package LeetCode.IntermediateAlgorithm.TreeAndMap;
+
+/**
+ * 给定一个二叉树
+ * <p>
+ * struct TreeLinkNode {
+ * TreeLinkNode *left;
+ * TreeLinkNode *right;
+ * TreeLinkNode *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * 说明:
+ * <p>
+ * 你只能使用额外常数空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 你可以假设它是一个完美二叉树（即所有叶子节点都在同一层，每个父节点都有两个子节点）。
+ */
+public class Connect {
+    /**
+     * Definition for binary tree with next pointer.
+     * public class TreeLinkNode {
+     * int val;
+     * TreeLinkNode left, right, next;
+     * TreeLinkNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public void connect(TreeLinkNode root) {
+
+        }
+    }
+}
---
+++ src/LeetCode/IntermediateAlgorithm/TreeAndMap/TreeLinkNode.java
@@ -0,0 +1,10 @@
+package LeetCode.IntermediateAlgorithm.TreeAndMap;
+
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
