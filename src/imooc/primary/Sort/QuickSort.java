e31790b38d181f4a6c0c91fc6129850eafbccffb
bert_cai <bert_cai@163.com>
2018-11-20 12:22:49 +0800

    优化了快速排序，现在通过随机取值作为中间值得方法分割待排序数组

--- src/imooc/primary/Sort/QuickSort.java
+++ src/imooc/primary/Sort/QuickSort.java
@@ -9,6 +9,9 @@ public class QuickSort {
         if (l >= r) {
             return;
         }
+        if (r - l < 15) { //待排序数量比较小时使用插入排序优化
+            InsertionSort.sort2(arr, l, r);
+        }
         int p = partition(arr, l, r);
         quickSort(arr, l, p - 1);
         quickSort(arr, p + 1, r);
@@ -16,12 +19,17 @@ public class QuickSort {
 
     /**
      * 快速排序的递归子过程，将给定范围的数组分为[l,p-1] [p+1,r]两部分
+     *
      * @param arr 待排序数组
-     * @param l 排序区间
-     * @param r 排序区间
+     * @param l   排序区间
+     * @param r   排序区间
      * @return 中间值得索引
      */
     private static int partition(Comparable[] arr, int l, int r) {
+        int randomIndex = (int) (Math.random() * (r - l)); // 随机取一个数作为中间值索引
+        Comparable temp1 = arr[randomIndex];
+        arr[randomIndex] = arr[l];
+        arr[l] = temp1;
         Comparable midValue = arr[l];
         int j = l;
         int i = l + 1;
